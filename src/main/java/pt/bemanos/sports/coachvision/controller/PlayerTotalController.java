bf3cb0931962a8f7e5df4c83fefa2f4e41333cd3
bercait <tacb14@gmail.com>
2025-01-12 12:23:32 +0000

    feat: Add player total scene

---
+++ src/main/java/pt/bemanos/sports/coachvision/controller/PlayerTotalController.java
@@ -0,0 +1,36 @@
+package pt.bemanos.sports.coachvision.controller;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import pt.bemanos.sports.coachvision.services.DynamicTableService;
+
+import java.net.URL;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class PlayerTotalController implements Initializable {
+    private final DynamicTableService dynamicTableService = new DynamicTableService();
+    @FXML
+    private AnchorPane playersTotalPane;
+    private TableView<Map<String, String>> playersTable;
+    private ObservableList<Map<String, String>> observableList;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        playersTable = dynamicTableService.createTableViewFromDynamicTable("");
+
+        AnchorPane.setTopAnchor(playersTable, 0d);
+        AnchorPane.setBottomAnchor(playersTable, 0d);
+        AnchorPane.setLeftAnchor(playersTable, 0d);
+        AnchorPane.setRightAnchor(playersTable, 0d);
+
+
+        playersTotalPane.getChildren().add(playersTable);
+        observableList = playersTable.getItems();
+
+
+    }
+}
