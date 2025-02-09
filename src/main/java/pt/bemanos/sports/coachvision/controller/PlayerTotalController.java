0077c8cf9c55453bf8ecfa64da37f864ae347b81
bercait <tacb14@gmail.com>
2025-02-09 18:57:26 +0000

    feat: Move tableview create to controller

--- src/main/java/pt/bemanos/sports/coachvision/controller/PlayerTotalController.java
+++ src/main/java/pt/bemanos/sports/coachvision/controller/PlayerTotalController.java
@@ -1,14 +1,19 @@
 package pt.bemanos.sports.coachvision.controller;
 
 import atlantafx.base.theme.Styles;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import pt.bemanos.sports.coachvision.services.DynamicTableService;
 
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
@@ -21,7 +26,7 @@ public class PlayerTotalController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        playersTable = dynamicTableService.createTableViewFromDynamicTable("player_total");
+        playersTable = createTableViewFromDynamicTable();
 
         AnchorPane.setTopAnchor(playersTable, 0d);
         AnchorPane.setBottomAnchor(playersTable, 0d);
@@ -33,5 +38,39 @@ public class PlayerTotalController implements Initializable {
 
         playersTotalPane.getChildren().add(playersTable);
         observableList = playersTable.getItems();
+    }
+
+    private TableView<Map<String, String>> createTableViewFromDynamicTable() {
+        List<Map<String, String>> data = dynamicTableService.getDataFromDynamicTable("player_total");
+        TableView<Map<String, String>> tableView = new TableView<>();
+
+        data.get(0).forEach((key, value) -> {
+            TableColumn<Map<String, String>, String> column = new TableColumn<>(key);
+            column.setCellValueFactory(map -> new SimpleStringProperty(map.getValue().get(key)));
+            tableView.getColumns().add(0, column);
+        });
+
+        TableColumn<Map<String, String>, String> column = new TableColumn<>("%\nRem");
+        column.setCellValueFactory(map ->
+                new SimpleStringProperty(calculatePercentage(map.getValue().get("Goals"), map.getValue().get("Throws"))));
+        tableView.getColumns().add(column);
+
+
+        ObservableList<Map<String, String>> observableList = FXCollections.observableList(data);
+        tableView.setItems(observableList);
+        return tableView;
+    }
+
+    private String calculatePercentage(String valueStr, String totalStr) {
+        String result = "";
+        float value = Float.parseFloat(valueStr);
+        float total = Float.parseFloat(totalStr);
+
+        DecimalFormat df = new DecimalFormat("0");
+        df.setMaximumFractionDigits(0);
+        if (total > 0) {
+            result = String.format(df.format(value / total * 100));
+        }
+        return result;
     }
 }
